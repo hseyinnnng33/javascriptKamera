4bd7c3ad085f05930f48af58c8cfda00571e722d
Hüseyin Aydın <110617469+hseyinnnng33@users.noreply.github.com>
2023-10-28 18:45:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const video = document.querySelector("#videoElement");
+const btn = document.querySelector(".fotoCek")
+const canvas = document.querySelector("#canvas")
+const canvas1 = document.querySelector("#canvas1")
 
 function startCamera() {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-          video.srcObject = stream;
-        })
+
+    // tarayıcının kullanıcıdan kamera ve mikrofon gibi cihazlara erişim izni talep etmesini sağlar.
+    navigator.mediaDevices.getUserMedia(
+        // kameraya erişim izni alınırken sadece video akışına izin verilmesini belirtir.
+        { video: true }
+    )
+    // izin verildiğinde yani kullanıcı kameraya erişim izni verdiğinde çalışan bir fonksiyondur.
+    .then(function (stream) {
+
+        // tarayıcıdan gelen akışı video elementine atar
+        video.srcObject = stream;
+    })
 }
+
+startCamera()
+
+btn.addEventListener("click", function(){
+    canvas1.getContext('2d').drawImage(video, 0, 0, canvas1.width, canvas1.height);
+})
+
+setInterval(function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas1.width, canvas1.height);
+}, 1);
+
